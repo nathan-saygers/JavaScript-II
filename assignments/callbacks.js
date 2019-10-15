31ddb6fd694e4db4b631598b795b0cf8c36242bc
nathan-saygers <nathan.saygers@gmail.com>
2019-10-15 13:39:59 -0700

    Got started on callback

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,11 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works
+// with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+function testStuff(value){
+  return value;
+}
 
 /* 
 
@@ -12,9 +17,9 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // SOLUTION:
 
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
+  // function firstItem(arr, cb) {
+  //   return cb(arr[0]);
+  // }
 
   // NOTES ON THE SOLUTION:
 
@@ -38,22 +43,33 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+console.log(getLength(items, testStuff));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+console.log(last(items, testStuff));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+console.log(sumNums(2, 5, testStuff));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+console.log(multiplyNums(4, 5, testStuff));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
